b201172a6f48058ce2fd161ff2f581c719a1df70
daragmas <daragmas@gmail.com>
2022-07-06 10:05:23 -0400

    commit tree 645446bc81515261922723d99b8d46f3306c9954

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    return customerName = 'BOB'
+}
+
+var bestCustomer
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'Tim'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Paul'
+}
